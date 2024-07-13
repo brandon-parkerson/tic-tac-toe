222adaecdfb5a925c2ed26009f1d374081dab311
brandon-parkerson <brandon-m-parkerson@gmail.com>
2024-07-13 19:26:37 +0300

    Create gameboard module. Create a factory function creating a player.

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 const Gameboard = (() => {
-    let board = [
-        "", "", "",
-        "", "", "",
-        "", "", "",
-    ];
+    let gameboard = ["", "", "", "", "", ""];
+
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
+        })
+    }
+    document.querySelector("#gameboard").innerHTML = boardHTML;
+
+    return {
+        render,
+    }
+    
 })();
 
-const Player = (name, mark) => {
-    return {name, mark};
-};
+const createPlayer = (name, marker) => {
+    return {
+        name, 
+        mark
+    }
+}
+
+
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;  
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "x"),
+            createPlayer(document.querySelector("#player2", "o"))
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    }
+})();
+
+
+
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+    Game.start();
+});
 
 
 
